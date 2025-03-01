88e4cfb95ba0df304a85af4391610fb1f37f6d5f
Sugesh Aruchamy <a.sugesh@hotmail.com>
2025-03-02 00:12:50 +0530

    Components added

--- ValuationClientApp/src/app/app.component.spec.ts
+++ ValuationClientApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ValuationClientApp' title`, () => {
+  it(`should have the 'demo1' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ValuationClientApp');
+    expect(app.title).toEqual('demo1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ValuationClientApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, demo1');
   });
 });
--- ValuationClientApp/src/app/app.component.ts
+++ ValuationClientApp/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeModeService } from './core/services/theme-mode.service';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ValuationClientApp';
+  title = 'demo1';
+
+  constructor(private themeModeService: ThemeModeService) {}
+
 }

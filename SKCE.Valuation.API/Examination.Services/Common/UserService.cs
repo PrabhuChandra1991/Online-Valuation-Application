79f045043c8a44c6cd1f60cc5b6a1516febcf3fd
venkat <venky27585@gmail.com>
2025-03-02 13:31:21 +0530

    Cleanup: Remove unused using directives in UserService.cs
    Removed several unnecessary `using` directives to enhance code readability and reduce clutter. The following namespaces were deleted: `System`, `System.Collections.Generic`, `System.Configuration`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`.

--- SKCE.Valuation.API/Examination.Services/Common/UserService.cs
+++ SKCE.Valuation.API/Examination.Services/Common/UserService.cs
@@ -3,12 +3,6 @@ using Examination.Models.DBModels.Common;
 using Examination.Services.ServiceContracts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Examination.Services.Common
 {

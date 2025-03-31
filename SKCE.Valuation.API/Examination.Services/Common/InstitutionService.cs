5453d61023ae23d9ce27bca1fd7e9fd4146a4783
Solutionarchitect@parkisolutions.com <solutionarchitect@parkisolutions.com>
2025-03-31 09:44:16 +0530

    edit assignment and added institute filter in assignment grid

---
+++ SKCE.Valuation.API/Examination.Services/Common/InstitutionService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SKCE.Examination.Models.DbModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKCE.Examination.Services.Common
+{
+    public class InstitutionService
+    {
+        private readonly ExaminationDbContext _context;
+
+        public InstitutionService(ExaminationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Institution>> GetAllInstitutionsAsync()
+        {
+            return await _context.Institutions.ToListAsync();
+        }
+    }
+}
---
+++ SKCE.Valuation.API/SKCE.Valuation.API/Controllers/Common/InstitutionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SKCE.Examination.Models.DbModels.Common;
+using SKCE.Examination.Services.Common;
+
+namespace SKCE.Examination.API.Controllers.Common
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstitutionController : ControllerBase
+    {
+        private readonly InstitutionService _institutionService;
+
+        public InstitutionController(InstitutionService institutionService)
+        {
+            _institutionService = institutionService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Institution>>> GetAllInstitutes()
+        {
+            return Ok(await _institutionService.GetAllInstitutionsAsync());
+        }
+    }
+}

faee5c50d1e281117d05162ff058149d8367dd81
Sakthivel <spsakthivelsty@gmail.com>
2025-04-18 16:02:02 +0530

    total marks calculation with choice
    total marks calculation with choice

--- ValuationClientApp/src/app/views/pages/apps/evaluation/evaluate/evaluate.component.ts
+++ ValuationClientApp/src/app/views/pages/apps/evaluation/evaluate/evaluate.component.ts
@@ -2,7 +2,6 @@ import { NgClass, NgFor } from '@angular/common';
 import { OnInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
-import { Question } from '../../../models/question.model';
 import { ToastrService } from 'ngx-toastr';
 import { EvaluationService } from '../../../services/evaluation.service';
 import { encode, decode } from 'js-base64';
@@ -16,12 +15,10 @@ import { ActivatedRoute } from '@angular/router'
 })
 
 export class EvaluateComponent implements OnInit {
-  isValue: any;
-  hasQuestion: boolean = false;
   primaryData: any;
   qaList: any[] = [];
-  part: any;
   partList: any[] = [];
+  groupList: any[] = [];
   totalMarks: number = 0;
   obtainedMarks: number = 0;
   activeQuestion: string = '---------- Please select the question above to load here ----------';
@@ -54,24 +51,25 @@ export class EvaluateComponent implements OnInit {
     this.evaluationService.getQuestionAndAnswer(answerSheetId).subscribe(
       (data: any[]) => {
         if (data.length > 0) {
-          let quest = {};
+
+          this.getPartList(data);
+          this.getGroupList(data);
+
           data.forEach((item) => {
-            let mark = 0;
-            const dom = new DOMParser().parseFromString(decode(item.questionMark), 'text/html');
-            let spans = dom.querySelectorAll('span');
-            spans.forEach((span) => {
-              let innerHTML = span.innerHTML.replace(/&nbsp;/g, '').trim();
-              if ((innerHTML.length > 0)) {
-                mark = parseInt(innerHTML);
-              }
+            this.qaList.push({
+              "questionId": item.questionNumber,
+              "questionPart": item.questionPartName,
+              "questionGroup": item.questionGroupName,
+              "questionNumber": item.questionNumberDisplay,
+              "questionDescription": decode(item.questionDescription),
+              "questionImage": decode(item.questionImage),
+              "answerDescription": decode(item.answerDescription),
+              "mark": this.getMark(item.questionMark)
             });
-            this.totalMarks = this.totalMarks + mark;
-            quest = { "questionId": item.questionNumber, "questionPart": item.questionPartName, "questionNumber": item.questionNumberDisplay, "questionDescription": decode(item.questionDescription), "questionImage": decode(item.questionImage), "answerDescription": decode(item.answerDescription), "mark": mark, "obtainedMark": 0, "hasOption": "" }
-            this.qaList.push(quest);
-            this.hasQuestion = true;
           });
+
+          this.reduceChoiceQuestionMarks();
           console.log("qaList: ", this.qaList);
-          this.getPartList();
         }
       },
       (error) => {
@@ -81,23 +79,60 @@ export class EvaluateComponent implements OnInit {
     );
   }
 
-  getPartList() {
+  getMark(questionMark: string) {
+    let mark = 0;
+    const dom = new DOMParser().parseFromString(decode(questionMark), 'text/html');
+    let spans = dom.querySelectorAll('span');
+    spans.forEach((span) => {
+      let innerHTML = span.innerHTML.replace(/&nbsp;/g, '').trim();
+      if ((innerHTML.length > 0)) {
+        mark = parseInt(innerHTML);
+      }
+    });
+    this.totalMarks = this.totalMarks + mark;
+
+    return mark;
+  }
+
+  reduceChoiceQuestionMarks() {
+     // Remove marks if choice question
+     this.groupList.forEach((group) => {    
+      if (group.count == 2) {
+        let item = this.qaList.find(e => e.questionGroup === group.group);
+        if (item) {
+          this.totalMarks = this.totalMarks - item.mark;
+          return;
+        }
+      }
+      if (group.count == 4) {
+        let item = this.qaList.find(e => e.questionGroup === group.group);
+        if (item) {
+          this.totalMarks = this.totalMarks - (item.mark * 2);
+          return;
+        }
+      }
+    });
+  }
+
+  getPartList(data: any[]) {
     console.log("getPartList...............");
     const partMap = new Map();
-    this.qaList.forEach(({ questionPart }) => {
-      partMap.set(questionPart, (partMap.get(questionPart) || 0) + 1);
+    data.forEach(({ questionPartName }) => {
+      partMap.set(questionPartName, (partMap.get(questionPartName) || 0) + 1);
     });
     this.partList = Array.from(partMap, ([part, count]) => ({ part, count }));
     console.log("partList:", this.partList);
+  }
 
-    // let parts = [...new Set(this.qaList.map(item => item.questionPart))];
-    // console.log("parts", parts);
-    // parts.forEach(item => {
-    //   let count = this.qaList.filter(e => e.questionPart === item).length;
-    //   this.part = { "part": item, "count": count }; 
-    //   this.partList.push(this.part);
-    // });
-    // console.log("partList: " + this.partList);
+  getGroupList(data: any[]) {
+    console.log("getGroupList...............");
+    const groupMap = new Map();
+    data.forEach(({ questionGroupName }) => {
+      groupMap.set(questionGroupName, (groupMap.get(questionGroupName) || 0) + 1);
+    });
+    this.groupList = Array.from(groupMap, ([group, count]) => ({ group, count }))
+      .filter(({ count }) => count > 1);
+    console.log("groupList:", this.groupList);
   }
 
   getQuestionDetails(event: any) {
@@ -122,9 +157,7 @@ export class EvaluateComponent implements OnInit {
     event.srcElement.classList.add('active');
     txt?.classList.add('active');
 
-    this.isValue = event.target.innerText;
-    //console.log(this.isValue);
-    let matchedItem = this.qaList.filter(x => x.questionNumber == this.isValue)[0];
+    let matchedItem = this.qaList.filter(x => x.questionNumber == event.target.innerText)[0];
     //console.log(matchedItem);
     this.activeQuestion = matchedItem.questionDescription;
     this.activeQuestionImg = matchedItem.questionImage;
@@ -153,38 +186,42 @@ export class EvaluateComponent implements OnInit {
     // btn?.classList.add('active');
   }
 
-  validateMark(event: any) {
+  validateMark(event: any, maxMark: number) {
     if (event.target.value) {
       if (event.target.value.match(/[^0-9]/g)) {
         this.toastr.error('Please add only numbers.');
         event.target.value = event.target.value.replace(/[^\d]/g, '');
         return;
       }
+      else if ((parseFloat(event.target.value) > parseInt(event.srcElement.max))) {
+        let msg = `Maximum marks allowed is ${maxMark}`
+        this.toastr.error(msg);
+        event.target.value = '';
+        return;
+      }
+
       //console.log(event.target.value + " > " + event.srcElement.max + " = " + (parseFloat(event.target.value) > parseInt(event.srcElement.max)));
       //console.log(event.target.value + " < " + event.srcElement.max + " = " + (parseFloat(event.target.value) < parseInt(event.srcElement.max)));
       //console.log((parseFloat(event.target.value) < parseInt(event.srcElement.max)));
       //set textbox color red if alloted mark greater than the question mark
-      if ((event.target.value < 0) || (parseFloat(event.target.value) > parseInt(event.srcElement.max))) {
-        event.srcElement.classList.add('error');
-      }
-      else {
-        event.srcElement.classList.remove('error');
-      }
+      // if ((event.target.value < 0) || (parseFloat(event.target.value) > parseInt(event.srcElement.max))) {
+      //   event.srcElement.classList.add('error');
+      // }
+      // else {
+      //   event.srcElement.classList.remove('error');
+      // }
       this.obtainedMarks = 0;
       let txtList = document.querySelectorAll(".question .form-control") as NodeListOf<HTMLInputElement>;
       txtList.forEach((item: HTMLInputElement) => {
         if (item.value) {
           this.obtainedMarks += parseFloat(item.value); // sum all text box marks
         }
-        else {
-          item.classList.remove('error'); // remove red color if the mark is removed or empty
-        }
       });
     }
   }
 
   submitEvaluation() {
-    if(this.obtainedMarks == 0) {
+    if (this.obtainedMarks == 0) {
       this.toastr.warning('Obained Marks is 0. Please evaluate.');
     }
     else if (this.obtainedMarks > this.totalMarks) {
